f8b9ca3a5190e9b08f2a63c56acbd179509090b7
ABHIJITH-EA <abhijithea3773@gmail.com>
2022-11-18 11:46:02 +0530

    Logging system added

--- src/app.py
+++ src/app.py
@@ -1,6 +1,19 @@
 import os
+import logging
+
 
 ENTRY_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(ENTRY_DIR)
 
-LOGFILE_NAME = 'application.py'
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+RESOURCE_DIR = os.path.join(BASE_DIR, 'resources')
+
+LOGFILE = os.path.join(DATA_DIR, 'application.log')
+
+# Logging system configuration
+logging.basicConfig(filename=LOGFILE,
+                    filemode='a',
+                    format='%(asctime)s %(process)s %(pathname)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    style='%',
+                    level='DEBUG')
