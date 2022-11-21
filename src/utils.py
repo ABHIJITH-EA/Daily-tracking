0cbb76c6daa3258a4cdaf51d08da387cd9152766
ABHIJITH-EA <abhijithea3773@gmail.com>
2022-11-21 17:06:45 +0530

    config toml added

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
 import os
 import config
 
+try:
+    import tomlib
+except ModuleNotFoundError:
+    import tomli as tomlib
+
 
 def get_log_file(filename: str) -> str:
     return os.path.join(config.DATA_DIR, filename)
