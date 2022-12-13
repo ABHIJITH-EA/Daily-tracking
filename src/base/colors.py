8cde12b2d9ac987d9bbd9f19fc9f000586616fe8
ABHIJITH-EA <abhijithea3773@gmail.com>
2022-12-13 18:43:44 +0530

    color module

---
+++ src/base/colors.py
@@ -0,0 +1,27 @@
+""" Color module """
+
+from logger import logger
+
+class _Color:
+    default = '0xffffff'
+    def __init__(self, value: tuple):
+        self.rgb = value
+
+    def hex_value(self) -> str:
+        try:
+            return f'{self.rgb[0]:02x}{self.rgb[1]:02x}{self.rgb[2]:02x}'
+        except Exception as e:
+            logger.info(f"can't convert to hex value getting error: {e}")
+            return _Color.default
+
+    @property
+    def value(self):
+        return self.rgb
+
+class Colors:
+    RED = _Color((255, 0, 0))
+    GREEN = _Color((0, 255, 0))
+    BLUE = _Color((0, 0, 255))
+
+    WHITE = _Color((255, 255, 255))
+    BLACK = _Color((0, 0, 0))
