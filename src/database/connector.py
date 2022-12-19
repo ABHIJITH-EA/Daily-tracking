91e55386170bea863fbebd75eeb61953fa6e0454
ABHIJITH-EA <abhijithea3773@gmail.com>
2022-12-19 12:21:07 +0530

    db connector

--- src/database/connector.py
+++ src/database/connector.py
@@ -7,17 +7,22 @@ from logger import logger
 _db_driver_instances = {}
 _DB_DRIVER_PATH = os.path.dirname(__file__).rpartition('/')[-1] + '.driver.'
 
+
 def _driver_initializer(driver: str):
     db_driver_path = _DB_DRIVER_PATH +  driver
+    driver_suffix = 'Db'
+    db_class = driver.capitalize() + driver_suffix
     try:
         driver_module = importlib.import_module(db_driver_path)
+        driver_instance = getattr(driver_module, db_class)
     except ModuleNotFoundError:
-        driver_module = None
+        driver_instance = None
         logger.warn(f'No {driver} driver found')
-    
-    return driver_module
+
+    return driver_instance
 
 
+# BUG: Why return a None db connection ?
 def connect(driver:str = 'mysql'):
     global _db_driver_instances
     if _db_driver_instances.get(driver):
